efdeba4877b40954f27d88da2139b074ae8d872d
baedonguri <rmsdbraos21@naver.com>
2022-04-25 22:39:55 +0900

    0425

--- week04/08_2098_외판원순회.py
+++ week04/08_2098_외판원순회.py
@@ -2,10 +2,30 @@ import sys
 input = sys.stdin.readline
 INF = float("inf")
 n = int(input())
-cities = [list(map(int, input().split())) for _ in range(n)]
-
+graph = [list(map(int, input().split())) for _ in range(n)]
+dp = [[INF]*(1<<n) for _ in range(n)]
 
 
 def dfs(x, visited):
-    if visited == (1<<n) - 1:
-        
+    if visited == (1<<n) - 1: # 모든 도시를 방문했을 때
+        if graph[x][0]: # 출발점으로 가는 경로가 있을 경우
+            return graph[x][0] # 출발점으로 가는 비용을 반환
+        else:
+            return INF # 출발점으로 가는 경우가 없다면 유효하지 않은 경로
+
+    if dp[x][visited] != INF: # 이미 최소비용이 계산되어 있다면
+        return dp[x][visited]
+    
+    for i in range(1,n): # 모든 도시를 탐방
+        if not graph[x][i]: # 가는 경로가 없다면 skip
+            continue
+        if visited & (1 << i): # 이미 방문한 도시라면 skip
+            continue
+
+        dp[x][visited] = min(dp[x][visited], dfs(i, visited | (1<<i)) + graph[x][i])
+    
+    return dp[x][visited]
+
+print(dfs(0,1))
+# for i in dp:
+#     print(i)
