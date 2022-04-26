bfef5435b919a482266522a407649e9632ef2291
baedonguri <rmsdbraos21@naver.com>
2022-04-26 22:33:05 +0900

    0426

--- week04/그리디/2720_세탁소사장동혁.py
+++ week04/그리디/2720_세탁소사장동혁.py
@@ -5,7 +5,12 @@ coins = [25,10,5,1]
 
 for _ in range(int(input())):
     c = int(input())
-    
-    while True:
+    table = []
+
+    for coin in coins:
+        table.append(c//coin)
+        c %= coin
         
+    print(*table)
+
     
