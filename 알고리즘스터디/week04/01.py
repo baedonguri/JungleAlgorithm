5eda3b27731de79568fcd8ef3ec2bc5474b3c624
baedonguri <rmsdbraos21@naver.com>
2022-05-21 20:24:39 +0900

    0521

--- 알고리즘스터디/week04/01.py
+++ 알고리즘스터디/week04/01.py
@@ -1,22 +1,36 @@
-def solution(routes):
-    routes.sort(key=lambda x:x[0])
-    routes.sort(key=lambda x:x[1])    
+# def solution(routes):
+#     routes.sort(key=lambda x:x[0])
+#     routes.sort(key=lambda x:x[1])    
 
+#     cnt = 1
+#     f_out = routes[0][1]
+#     flag = True
+
+#     for route in routes[1:]:
+#         s_in, s_out = route        
+#         if s_out >= f_out >= s_in:
+#             flag = False
+#         else:
+#             flag = True
+#             f_out = s_out
+
+#         if flag: cnt += 1
+#     return cnt
+
+# routes = [[-20,-15], [-14,-5], [-18,-13], [-5,-3]]
+
+def solution(routes):
+    routes.sort(key=lambda x:x[1])
     cnt = 1
     f_out = routes[0][1]
-    flag = True
 
     for route in routes[1:]:
         s_in, s_out = route        
-        if s_out >= f_out >= s_in:
-            flag = False
-        else:
-            flag = True
+        if not (s_out >= f_out >= s_in):
+            cnt += 1
             f_out = s_out
-
-        if flag: cnt += 1
+        
     return cnt
 
-routes = [[-20,-15], [-14,-5], [-18,-13], [-5,-3]]
 
-print(solution(routes))
+print(solution([[-20,-15], [-14,-5], [-18,-13], [-5,-3]]))
