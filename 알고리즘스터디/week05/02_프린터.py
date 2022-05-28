3c1fcb5619657feb22abe78db3e47fdd9897a711
baedonguri <rmsdbraos21@naver.com>
2022-05-28 20:29:14 +0900

    프린터 재풀이

--- 알고리즘스터디/week05/02_프린터.py
+++ 알고리즘스터디/week05/02_프린터.py
@@ -2,25 +2,37 @@
 # https://programmers.co.kr/learn/courses/30/lessons/42587
 from collections import deque
 
+# def solution(priorities, location):
+#     que = deque([(idx, val) for idx, val in enumerate(priorities)])
+#     answer = 0
+
+#     while que:
+#         flag = True
+#         q = que.popleft()
+#         for i in que:
+#             if i[1] > q[1]:
+#                 flag = False
+#         if flag:
+#             answer += 1
+#             if q[0] == location:
+#                 break
+#         else:
+#             que.append(q)
+
+    # return answer
 def solution(priorities, location):
-    que = deque([(idx, val) for idx, val in enumerate(priorities)])
+    que = deque([(val, idx) for idx, val in enumerate(priorities)])
     answer = 0
 
     while que:
-        flag = True
         q = que.popleft()
-        for i in que:
-            if i[1] > q[1]:
-                flag = False
-        if flag:
-            answer += 1
-            if q[0] == location:
-                break
-        else:
+        if max(que)[0] > q[0]:
             que.append(q)
-
-    return answer
-
+        else:
+            answer += 1
+            if q[1] == location:
+                return answer
+    
 priorities = [2, 1, 3, 2]
 location = 2
 print(solution(priorities, location))
